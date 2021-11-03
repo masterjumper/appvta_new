a28360ac31debef55ce9b6ec34936bb0f2f78629
Materjumper <masterjumper@hotmail.com>
2021-11-03 20:24:10 -0300

    doi

--- application/views/producto/new_producto.php
+++ application/views/producto/new_producto.php
@@ -1,18 +1,32 @@
 <div class="container">
     <body>
     <div class="sign-up-form form">
-        <?php echo form_open(base_url().'produccion/save/'); ?>
+        <?php echo form_open(base_url().'producto/save/'); ?>
         <label class="form-label-wrapper">
-            <p class="form-label">Sistema de Produccion </p>
-            <input class="form-input" type="input" required name="SistemaProduccionDescripcion" value="<?php echo set_value('SistemaProduccionDescripcion') ; ?>">
+            <p class="form-label">Codigo </p>
+            <input class="form-input" type="input" required name="procodbar" value="<?php echo set_value('procodbar') ; ?>">
         </label>
         <div class="stat-cards-info__profit danger">
-            <?php echo form_error('SistemaProduccionDescripcion'); ?>
+            <?php echo form_error('procodbar'); ?>
+        </div>
+        <label class="form-label-wrapper">
+            <p class="form-label">Producto </p>
+            <input class="form-input" type="input" required name="prodsc" value="<?php echo set_value('prodsc') ; ?>">
+        </label>
+        <div class="stat-cards-info__profit danger">
+            <?php echo form_error('prodsc'); ?>
+        </div>
+        <label class="form-label-wrapper">
+            <p class="form-label">Importe </p>
+            <input class="form-input" type="input" required name="proimp" value="<?php echo set_value('proimp') ; ?>">
+        </label>
+        <div class="stat-cards-info__profit danger">
+            <?php echo form_error('proimp'); ?>
         </div>
         <button class="form-btn primary-default-btn transparent-btn">Guardar</button>
         <?=form_close()?>
         <p class="form-label"></p>
-        <a href=<?php echo (base_url()."produccion/index/");?>>
+        <a href=<?php echo (base_url()."producto/index/");?>>
             <submit type="submit" class="form-btn primary-default-btn transparent-btn"> Cancelar</submit>
         </a>
     </div>
--- application/views/producto/producto.php
+++ application/views/producto/producto.php
@@ -1,21 +1,35 @@
 <div class="container">
     <body>
     <div class="sign-up-form form">
-        <?php echo form_open(base_url().'produccion/save/'); ?>
-        <?php foreach($produccion as $item) { ?>
-            <input class="form-input" type="input" name="idSistemaProduccion" readonly hidden="true" value=<?php echo $item->idSistemaProduccion;?>>
+        <?php echo form_open(base_url().'producto/save/'); ?>
+        <?php foreach($producto as $item) { ?>
+            <input class="form-input" type="input" hidden="true" name="proid" value="<?php echo $item->proid;?>">
             <label class="form-label-wrapper">
-                <p class="form-label">Sistema de Produccion</p>
-                <input class="form-input" type="input" name="SistemaProduccionDescripcion" required value="<?php echo (set_value('SistemaProduccionDescripcion'))? set_value('SistemaProduccionDescripcion'): $item->SistemaProduccionDescripcion ; ?>">
+                <p class="form-label">Codigo </p>
+                <input class="form-input" type="input" required name="procodbar" value="<?php echo $item->procodbar;?>">
             </label>
             <div class="stat-cards-info__profit danger">
-                <?php echo form_error('SistemaProduccionDescripcion'); ?>
+                <?php echo form_error('procodbar'); ?>
+            </div>
+            <label class="form-label-wrapper">
+                <p class="form-label">Producto </p>
+                <input class="form-input" type="input" required name="prodsc" value="<?php echo $item->prodsc;?>">
+            </label>
+            <div class="stat-cards-info__profit danger">
+                <?php echo form_error('prodsc'); ?>
+            </div>
+            <label class="form-label-wrapper">
+                <p class="form-label">Importe </p>
+                <input class="form-input" type="input" required name="proimp" value="<?php echo $item->proimp;?>">
+            </label>
+            <div class="stat-cards-info__profit danger">
+                <?php echo form_error('proimp'); ?>
             </div>
         <?php } ?>
         <button class="form-btn primary-default-btn transparent-btn">Guardar</button>
         <?=form_close()?>
         <p class="form-label"></p>
-        <a href=<?php echo (base_url()."produccion/index/");?>>
+        <a href=<?php echo (base_url()."producto/index/");?>>
             <submit type="submit" class="form-btn primary-default-btn transparent-btn"> Cancelar</submit>
         </a>
     </div>
